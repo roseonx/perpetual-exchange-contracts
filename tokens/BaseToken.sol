69469418962287b492f306526c91c9ea6b29ac8e
mmo2112 <mmo2112@gmail.com>
2023-03-17 16:16:39 +0700

    add logic check minter role before grant/revoke, remove unused lib

--- tokens/BaseToken.sol
+++ tokens/BaseToken.sol
@@ -6,7 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 contract BaseToken is ERC20Burnable, Pausable, Ownable {
     using SafeERC20 for IERC20;
