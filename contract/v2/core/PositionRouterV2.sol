bb7eeac09dbeb4e3f2dc4280a4eff491791955d9
Phuc Le <mmo2112@gmail.com>
2023-10-18 19:18:12 +0700

    double check positionRouter will revert if path required swap

--- contract/v2/core/PositionRouterV2.sol
+++ contract/v2/core/PositionRouterV2.sol
@@ -493,7 +493,10 @@ contract PositionRouterV2_4 is BasePositionV2, IPositionRouterV2, ReentrancyGuar
             );
 
             return;
-        } 
+        } else if (_shouldSwap && address(swapRouter) == address(0)) {
+            //Currently not support swap ony UI
+            revert("Swap temporarily disabled");
+        }
 
         Position memory position;
         OrderInfo memory order;
